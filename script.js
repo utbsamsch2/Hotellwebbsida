7d58ff6b1dcf54c03c4768575577f0ea1b20fe26
utbsamsch2 <112628077+utbsamsch2@users.noreply.github.com>
2023-06-05 10:22:04 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("submitButton").addEventListener("click", function(event) {
-  event.preventDefault(); 
-  
-  document.getElementById("fname").value = "";
-  document.getElementById("lname").value = "";
-  document.getElementById("Rumstyp").selectedIndex = 0;
-  document.getElementById("subject").value = "";
-});
